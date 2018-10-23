bfdf4269f8e41e129fdf05be97e14608aedcbb82
Thomas Vengels <>
2018-10-23 16:56:05 +0200

    no message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, TemplateRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  thenBlock: TemplateRef<any>|null = null;
+
+  show = true;
+
+  @ViewChild('primaryBlock')
+  primaryBlock: TemplateRef<any>|null = null;
+  @ViewChild('secondaryBlock')
+  secondaryBlock: TemplateRef<any>|null = null;
+
+  ngOnInit() {
+    console.log(this.primaryBlock);
+    this.thenBlock = this.primaryBlock;
+  }
+
+  toggle() {
+    this.show = !this.show;
+    this.thenBlock = (this.show)
+      ? this.primaryBlock
+      : this.secondaryBlock;
+  }
+
+  onClick(value) {
+    console.log(value);
+  }
 }
